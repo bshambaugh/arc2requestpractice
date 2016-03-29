0f68488e272a9a40b607bdf9eccc673677fdead4
brent <brent@brent-Inspiron-1525.(none)>
2016-03-29 18:43:39 -0400

    Added multiarraywkey which splits a multidimensional array into files

--- multiimplodewkey.php
+++ multiimplodewkey.php
@@ -14,18 +14,32 @@ $multiarray = array('key' => array('innerkey' => 'value'),'key2' => array('inner
 foreach($secondarray as $a => $b) {
   $subarray = array();
    $subarraymulti = array();
-  $filename = strval($a).'.txt';
+  $astring = strval($a).'.txt';
+  preg_match('/([^\\/])*txt/',$astring,$matches);
+  $filename = $matches[0];
+  echo($filename);
+  echo("\r\n");
+
   $fp = fopen($filename,'w');
   // echo("\r\n");
  foreach($b as $c => $d) {
 // echo($multiarray[$a][$c]);
- $subarray[$c] = $multiarray[$a][$c];
- $subarraymulti[$c] = strval($c).' '.strval($subarray[$c]);
-// echo($subarraymulti[$c]);
+//echo(strval($c).' '.strval($secondarray[$a][$c]));
+$subarraymulti[$c] = strval($c).' '.strval($secondarray[$a][$c]);
+//echo("\r\n");
+// $subarray[$c] = $secondarray[$a][$c];
+// echo strval($subarray[$c]);
+ //$subarraymulti[$c] = strval($c).' '.strval($subarray[$c]);
+ //echo($subarraymulti[$c]);
  }
  $withcomma = implode("\n",$subarraymulti);
+ echo($withcomma);
+ echo('-------');
+ echo("\r\n");
+//$withcomma = implode("\n",$subarraymulti);
  fwrite($fp, $withcomma);
  fclose($fp);
+ 
 }
 
 //print_r($subarray);
