fa74e136019b3a5d274577f697a965eb5e3aec59
bshambaugh <brent.shambaugh@gmail.com>
2016-03-31 20:07:56 -0400

    Modified the multiimplodewkey.php script so that it writes to a linked data platform container

--- multiimplodewkey.php
+++ multiimplodewkey.php
@@ -20,27 +20,28 @@ foreach($secondarray as $a => $b) {
   preg_match('/([^\\/])*ttl/',$astring,$matches);
   $filename = $matches[0];
   $containername = preg_replace('/\.ttl/','',$filename);
-
+  //echo $containername;
   echo($filename);
   // get the slug from the $filename by subtracting .ttl for postrequest.php here
-  echo("\r\n");
+//  echo("\r\n");
 
 // I am going to assume I do not need to create a file by commenting out below..
-//  $fp = fopen($filename,'rw');
+  $fp = fopen('./'.$filename,'w');
 
   // echo("\r\n");
  foreach($b as $c => $d) {
 // echo($multiarray[$a][$c]);
 //echo(strval($c).' '.strval($secondarray[$a][$c]));
-echo strval($c);
+//echo strval($c);
       if(preg_match('/dc\/terms\/date/', strval($c)) || preg_match('/dc\/terms\/created/', strval($c)) || preg_match('/dc\/terms\/modified/', strval($c)) == 1) {
-        echo "I have a date or I am created or modified";
+      //  echo "I have a date or I am created or modified";
       } else {
         if(preg_match('/rss\/1.0\/modules\/content\/encoded/', strval($c)) == 1) {
           $string = strval($secondarray[$a][$c]);
           $stringproc = preg_replace('/</','&lt;',$string);
           $stringproc2 = preg_replace('/>/','&gt;',$stringproc);
-          $subarraymulti[$c] = httpmatched(strval($c)).' '.httpmatched($stringproc2).' ;';
+          $stringproc3 = preg_replace('/\'/','\\\'',$stringproc2);
+          $subarraymulti[$c] = httpmatched(strval($c)).' '.httpmatched($stringproc3).' ;';
         } elseif (preg_match('/rss\/1.0\/modules\/content\/encoded/', strval($c)) != 1) {
            if(preg_match('/schema\.org\/name/', strval($c)) == 1) {
              $subarraymulti[$c] = httpmatched(strval($c)).' '.httpmatched(strval($secondarray[$a][$c])).' ;'."\n"
@@ -51,8 +52,8 @@ echo strval($c);
         }
 
       }
-echo(httpmatched(strval($c)));
-echo("\r\n");
+//echo(httpmatched(strval($c)));
+//echo("\r\n");
 //$subarraymulti[$c] = strval($c).' '.strval($secondarray[$a][$c]);
 
 //echo("\r\n");
@@ -65,20 +66,26 @@ echo("\r\n");
  // http://stackoverflow.com/questions/5592994/remove-the-last-character-from-string
  $withcommamid = substr($withcommapre, 0, -1);
  $withcomma = '<>'."\n".$withcommamid.'.';
- /*
+ echo $withcomma;
+/*
  echo($withcomma);
+ echo $containername;
  echo('-------');
  echo("\r\n");
- */
-//$withcomma = implode("\n",$subarraymulti);
+*/
+
+// $withcomma = implode("\n",$subarraymulti);
 // I am going to assume I do not need to create a file by commenting out below..
-// fwrite($fp, $withcomma);
+ fwrite($fp, $withcomma);
+
 
  // see if this works...otherwise close and reopen file and put the function somewhere else...
- postandputtoldp($containername,$withcomma);
+// postandputtoldp($containername,$withcomma);
  //run the putrequest2.php file contents here...
  // I am going to assume I do not need to create a file by commenting out below..
- // fclose($fp);
+  fclose($fp);
+
+ pushandput($filename);
 
 }
 
@@ -98,20 +105,67 @@ function httpmatched($string) {
   return $newstring;
 }
 
+/*
 function postandputtoldp($containertitle,$data) {
-$url = 'http://localhost:8080/marmotta/ldp/drupalsite/';
+$url = 'http://localhost:8080/marmotta/ldp/drupalsite2/';
 $headers = array('Content-Type' => 'text/turtle','Slug' => $containertitle);
 $response = Requests::post($url, $headers);
 
-$url = $url.'/'.$containertitle;
+$url = $url.$containertitle;
+//echo $url;
 
 $existingheaders = get_headers($url);
+//echo $existingheaders;
 
 $etag = preg_replace('/ETag: /i','',$existingheaders[5]);
+echo $etag;
 
 $headers = array('Content-Type' => 'text/turtle',
                  'If-Match' => $etag ,
                   'Slug' => $containertitle);
 
+echo($data);
+
 $response = Requests::put($url,$headers,$data);
+}
+*/
+
+//$inputfile = "joe-smith.ttl";
+
+//$containertitle = 'Let-us-build-a-Boat';
+
+//pushandput($inputfile);
+
+function pushandput ($inputfile) {
+
+$containertitle = preg_replace('/\.ttl/','',$inputfile);
+$url = 'http://localhost:8080/marmotta/ldp/drupalsite5/';
+$headers = array('Content-Type' => 'text/turtle','Slug' => $containertitle);
+$response = Requests::post($url, $headers);
+
+var_dump($response->body);
+
+?>
+
+<?php
+
+$handle = fopen($inputfile,'r');
+echo($handle);
+$data = fread($handle, filesize($inputfile));
+
+echo($data);
+$url = 'http://localhost:8080/marmotta/ldp/drupalsite5/'.$containertitle;
+$existingheaders = get_headers($url);
+print_r($existingheaders);
+echo($existingheaders[5]);
+$etag = preg_replace('/ETag: /i','',$existingheaders[5]);
+echo("\n");
+echo($etag);
+echo("\n");
+$headers = array('Content-Type' => 'text/turtle','If-Match' => $etag,'Slug' => $containertitle);
+//$headers = array('Content-Type' => 'text/turtle','If-Match' => 'W/"1459004153000"','Slug' => 'Penguins are Awesome');
+$response = Requests::put($url, $headers, $data);
+//$response = Requests:_put($url, $headers, json_encode($data));
+var_dump($response->body);
+fclose($handle);
 }
